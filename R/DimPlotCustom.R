176a439c0ba18e41d6587e108f714a737a970809
John Mariani <johnmariani@gmail.com>
2022-07-06 17:31:42 -0400

    Added check for levels in split.by

--- R/DimPlotCustom.R
+++ R/DimPlotCustom.R
@@ -11,7 +11,11 @@ DimPlotCustom <- function(seurat, group.by = "orig.ident", pt.size = 1, plotLege
       ggplot2::xlim(xLimits) + ggplot2::ylim(yLimits) + ggplot2::ylab("UMAP 2") + ggplot2::xlab("UMAP 1") + ggplot2::theme(plot.tag = element_text(size = 12), legend.position = "bottom", legend.direction = "horizontal", text = element_text(size = labelFont), legend.text = element_text(size = 6), plot.margin = unit(c(0,0,0,0), "cm"), plot.title = element_text(hjust = 0.5, size = titleFont))
   } else {
     splitDF <- seurat@meta.data[drop = F,,split.by]
-    splits <- unique(splitDF[,1])
+    if(!is.null(levels(splitDF[,1]))){
+      splits <- levels(splitDF[,1])
+    } else {
+      splits <- unique(splitDF[,1])
+    }
     p <- lapply(splits, function(y) {
       ggplot2::ggplot(data = embeddings[row.names(splitDF[splitDF[,split.by] %in% y,,drop = F]),], aes(x= UMAP_1, y=UMAP_2)) + ggplot2::geom_point(aes(fill= group), size = pt.size, colour = "black", stroke = .1, shape = 21) + ggplot2::theme_classic() +
         ggplot2::xlim(xLimits) + ggplot2::ylim(yLimits) + ggplot2::ylab("UMAP 2") + ggplot2::xlab("UMAP 1") + ggplot2::theme(plot.tag = element_text(size = 12), legend.position = "bottom", legend.direction = "horizontal", text = element_text(size = labelFont), legend.text = element_text(size = 6), plot.margin = unit(c(0,0,0,0), "cm"), plot.title = element_text(hjust = 0.5, size = titleFont)) + ggplot2::ggtitle(y) 
